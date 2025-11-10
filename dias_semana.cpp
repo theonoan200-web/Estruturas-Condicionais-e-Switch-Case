3aa622ac31908b2cb3a332b5174f22bcc4c23a83
theonoan200-web <theonoan200@gmail.com>
2025-11-10 17:16:47 -0300

    Verificação dos dias da semana

---
+++ dias_semana.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int dia
+    
+    cout<< "digite um numero de 1 a 7: ";
+    cin >> dia
+    
+    switch (dia) {
+    case 1:
+       cout << "domingo";
+    break;
+    
+    case 2:
+       cout << "segunda";
+    break
+       
+    case 3:
+       cout << "terça";
+    break
+    
+    case 4:
+       cout <<"quarta";
+    break
+       
+    case 5:
+       cout << "quinta";
+    break
+    
+    case 6:
+       cout << "sexta";
+    break
+    
+    case 7:
+       cout << "sabado";
+    break
+    
+    default: cout << "dia invalido";
+    }
