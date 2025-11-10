0cdc2f913b7456200a6c2f04e04ad2d7a4a23675
theonoan200-web <theonoan200@gmail.com>
2025-11-10 17:15:49 -0300

    Verificação de vogais e consoantes

---
+++ vogal_consoante.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    char letra;
+    
+    cout << "digite uma letra: ";
+    cin >> letra;
+    
+    switch (letra) {
+        case 'a': cout << "vogal A";
+        break
+        
+        case 'e': cout << "vogal E";
+        break
+        
+        case 'i': cout << "vogal I";
+        break
+        
+        case 'o': cout << "vogal O";
+        break
+        
+        case 'u': cout << "vogal U";
+        
+    }
