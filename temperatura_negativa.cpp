ca9b79cd922fc4061fda4a6b0155124495cf2d23
theonoan200-web <theonoan200@gmail.com>
2025-11-17 14:34:04 -0300

    Create temperatura_negativa.cpp

---
+++ temperatura_negativa.cpp
@@ -0,0 +1,21 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int idade;
+    
+    int temperatura;
+    
+    cout << " digite temperatura em celsiuls " << endl;
+    cin >> temperatura;
+    
+     if (temperatura < 0)  {
+        cout << "temperatura negativa! " 
+     }
+     else if (temperatura ==0 ) {
+         cout<< "ponto de congelamento!",
+    } 
+    else{ 
+        cout << " temperatura positiva " 
+
