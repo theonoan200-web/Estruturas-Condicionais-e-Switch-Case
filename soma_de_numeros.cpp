9ff4500bb184dc95b858c01444e7b575e1825e18
theonoan200-web <theonoan200@gmail.com>
2025-11-17 14:35:14 -0300

    Create soma_de_numeros.cpp

---
+++ soma_de_numeros.cpp
@@ -0,0 +1,17 @@
+1-Algoritmo "soma de numeros de 1 a 5"
+
+Var
+  i,soma:inteiro
+Inicio
+
+     soma<- 0
+     
+     para i 1 ate 5 faça
+           soma<-soma+1
+     
+     fimpara
+     
+     escreval("a soma e: ",soma)
+   
+Fimalgoritmo
+
